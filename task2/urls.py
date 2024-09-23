dfa874a036468192ebccb557078e15896fc4a9cc
Dolgiholeg <Dolgiholeg@rambler.ru>
2024-09-23 14:15:03 +0400

    Корректировка task2

--- task2/urls.py
+++ task2/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('class/', views.class_view, name='class_view'),
+    path('class/', views.class_view.as_view(), name='class_view'),
     path('function/', views.function_view, name='function_view'),
 ]
--- task2/views.py
+++ task2/views.py
@@ -1,8 +1,8 @@
-
+from  django.views.generic import TemplateView
 from django.shortcuts import render
 
-def class_view(request):
-    return render(request, 'second_task/class_template.html')
+class class_view(TemplateView):
+    template_name = 'second_task/class_template.html'
 
 def function_view(request):
     return render(request, 'second_task/func_template.html')
